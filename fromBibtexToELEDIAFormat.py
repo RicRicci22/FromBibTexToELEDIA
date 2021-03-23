44eee1645d31d86712b61fff081969307932b53d
Riccardo <riccardo.ricci-1@studenti.unitn.it>
2021-03-23 23:26:17 +0100

    Inserted DOI and created function get_author

--- fromBibtexToELEDIAFormat.py
+++ fromBibtexToELEDIAFormat.py
@@ -26,82 +26,16 @@ def fromBibtoEledia(arguments):
                 except Exception:
                     print("Error: 'author'/'editor' filed not found")
                     quit()
-            # DA QUA FUNZIONE
-            if len(author) == 1:
-                first_names = author[0].bibtex_first_names
-                last_names = author[0].last_names
-                for a in range(len(first_names)):
-                    if a == 0:
-                        string_names = first_names[a]
-                    else:
-                        string_names += " "+first_names[a]
-                for aa in range(len(last_names)):
-                    string_names += " " + \
-                        last_names[aa].replace('{', '').replace('}', '')
-                print(string_names)
-
-            elif len(author) == 2:  # DUE AUTORI
-                for aut in range(len(author)):
-                    if aut == 0:
-                        first_names = author[aut].bibtex_first_names
-                        last_names = author[aut].last_names
-                        for a in range(len(first_names)):
-                            if a == 0:
-                                string_names = first_names[a]
-                            else:
-                                string_names += " "+first_names[a]
-                        for aa in range(len(last_names)):
-                            string_names += " " + \
-                                last_names[aa].replace(
-                                    '{', '').replace('}', '')
-                    else:
-                        string_names += " and"
-                        first_names = author[aut].bibtex_first_names
-                        last_names = author[aut].last_names
-                        for a in range(len(first_names)):
-                            string_names += " "+first_names[a]
-                        for aa in range(len(last_names)):
-                            string_names += " " + \
-                                last_names[aa].replace(
-                                    '{', '').replace('}', '')
-            else:
-                for aut in range(len(author)):
-                    if aut == 0:
-                        first_names = author[aut].bibtex_first_names
-                        last_names = author[aut].last_names
-                        for a in range(len(first_names)):
-                            if a == 0:
-                                string_names = first_names[a]
-                            else:
-                                string_names += " "+first_names[a]
-                        for aa in range(len(last_names)):
-                            string_names+=" "+last_names[aa].replace('{','').replace('}','')
-                    elif aut==len(author)-1:
-                            string_names+= ", and"
-                            first_names=author[aut].bibtex_first_names
-                            last_names=author[aut].last_names
-                            for a in range(len(first_names)):
-                                string_names+=" "+first_names[a]
-                            for aa in range(len(last_names)):
-                                string_names+=" "+last_names[aa].replace('{','').replace('}','')
-                    else:
-                        string_names+= ", "
-                        first_names=author[aut].bibtex_first_names
-                        last_names=author[aut].last_names
-                        for a in range(len(first_names)):
-                            string_names += " "+first_names[a]
-                        for aa in range(len(last_names)):
-                            string_names += " " + \
-                                last_names[aa].replace(
-                                    '{', '').replace('}', '')
 
             # WRITE DATA ON FILE
             # Get volume
+            string_names = support_functions.get_author_name(author)
             volume = support_functions.get_volume(fields['volume'])
             number = support_functions.get_number(fields['number'])
             pages = support_functions.get_pages(fields['pages'])
+            doi = support_functions.get_doi(fields['doi'])
             f.write(
-                f"[{author[0].last_names[0].replace('{','').replace('}','')}.{fields['year']}] {string_names}, \"{fields['title']},\" {abbr[fields['journal']]}, {volume}{number}{pages}\n")
+                f"[{author[0].last_names[0].replace('{','').replace('}','')}.{fields['year']}] {string_names}, \"{fields['title']},\" {abbr[fields['journal']]}, {volume}{number}{pages}{doi}\n")
     f.close()
 
 
