d40853f1d28226356204ae66981666ae952f8c33
SergioPovoli <sergio.povoli@studenti.unitn.it>
2021-03-23 22:31:58 +0100

    2aut

--- fromBibtexToELEDIAFormat.py
+++ fromBibtexToELEDIAFormat.py
@@ -18,7 +18,7 @@ def fromBibtoEledia(arguments):
     for bib_data in bib_datas:
         for e in bib_data.entries:
             fields = bib_data.entries[e].fields
-            print(bib_data.entries[e].persons)
+            #print(bib_data.entries[e].persons)
             try:
                 author = bib_data.entries[e].persons['author']
             except Exception:
@@ -27,6 +27,7 @@ def fromBibtoEledia(arguments):
                 except Exception:
                     print("Error: 'author'/'editor' filed not found")
                     quit()
+            ## DA QUA FUNZIONE
             if len(author) == 1:
                 first_names=author[0].bibtex_first_names
                 last_names=author[0].last_names
@@ -38,14 +39,33 @@ def fromBibtoEledia(arguments):
                 for aa in range(len(last_names)):
                     string_names+=" "+last_names[aa].replace('{','').replace('}','')
                 print(string_names)
-            elif len(author) == 2:
-                string_names = "G. Oliveri and A. Massa"
+
+            elif len(author) == 2: #DUE AUTORI
+                for aut in range(len(author)):
+                    if aut==0:
+                        first_names=author[aut].bibtex_first_names
+                        last_names=author[aut].last_names
+                        for a in range(len(first_names)):
+                            if a==0:
+                                string_names=first_names[a]
+                            else:
+                                string_names+=" "+first_names[a]
+                        for aa in range(len(last_names)):
+                            string_names+=" "+last_names[aa].replace('{','').replace('}','')
+                    else:
+                        string_names+= " and"
+                        first_names=author[aut].bibtex_first_names
+                        last_names=author[aut].last_names
+                        for a in range(len(first_names)):
+                            string_names+=" "+first_names[a]
+                        for aa in range(len(last_names)):
+                            string_names+=" "+last_names[aa].replace('{','').replace('}','')
             else:
-                string_names = "G. Oliveri and A. Massa"
+                string_names = "PUTTANA"
 
             # WRITE DATA ON FILE
             f.write(
-                f"[{author[0].last_names[0].replace('{','').replace('}','')}.{fields['year']}] {string_names}, \"{fields['title']},\" {abbr[fields['journal']]}\n")
+                f"[{author[0].last_names[0].replace('{','').replace('}','')}.{fields['year']}] {string_names}, \"{fields['title']},\" {abbr[fields['journal']]}\n\n")
     f.close()
 
 
